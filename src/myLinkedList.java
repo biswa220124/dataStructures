489f95bac46a79e1be869a8ca05b93b52c1b7012
biswa <biswabhusan2190@gmail.com>
2025-10-26 09:16:25 +0530

    Added New Method for setting value at a particular index

--- src/myLinkedList.java
+++ src/myLinkedList.java
@@ -2,52 +2,58 @@ public class myLinkedList {
     private Node head;
     private Node tail;
     private int length;
-    class Node{
+
+    class Node {
         int value;
         Node next;
 
-        Node(int value){
+        Node(int value) {
             this.value = value;
         }
     }
-   myLinkedList(int value){
+
+    myLinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
-        length=1;
-   }
-   public void printLinkedList(){
+        length = 1;
+    }
+
+    public void printLinkedList() {
         Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.value+" ");
+        while (temp != null) {
+            System.out.print(temp.value + " ");
             temp = temp.next;
         }
-   }
-   public void getHead(){
-       System.out.println(head.value);
-   }
-   public void getTail(){
-       System.out.println(tail.value);
-   }
-   public void append(int value){
+    }
+
+    public void getHead() {
+        System.out.println(head.value);
+    }
+
+    public void getTail() {
+        System.out.println(tail.value);
+    }
+
+    public void append(int value) {
         Node newNode = new Node(value);
-       if(length==0){
-           head = newNode;
-           tail = newNode;
-   }
-       else{
-           tail.next = newNode;
-           tail = newNode;
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
 
-       }
-       length++;
+        }
+        length++;
 
-   }
-   public Node removeLast(){
-        if(length==0) return null;
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
         Node temp = head;
         Node pre = head;
-        while(temp.next!=null){
+        while (temp.next != null) {
             pre = temp;
             temp = temp.next;
         }
@@ -55,49 +61,60 @@ public class myLinkedList {
         tail.next = null;
         length--;
 
-        if(length==0){
-            head=null;
-            tail=null;
+        if (length == 0) {
+            head = null;
+            tail = null;
         }
         return temp;
 
-   }
-   public void prepend(int value){
+    }
+
+    public void prepend(int value) {
         Node newNode = new Node(value);
-        if(length==0){
+        if (length == 0) {
             head = newNode;
             tail = newNode;
-        } else{
+        } else {
             newNode.next = head;
             head = newNode;
         }
         length++;
-   }
-   public Node removeFirst(){
+    }
+
+    public Node removeFirst() {
         Node temp = head;
-        if(length==0) return null;
-        else{
+        if (length == 0) return null;
+        else {
             head = head.next;
             temp.next = null;
         }
         length--;
-        if(length==0){
-            tail=null;
+        if (length == 0) {
+            tail = null;
         }
         return temp;
-   }
-   public Node get(int index){
-       Node temp = head;
-        if(index<0 || index>=length) return null;
+    }
 
-        else{
-            for(int i=0; i<index;i++){
+    public Node get(int index) {
+        Node temp = head;
+        if (index < 0 || index >= length) return null;
+
+        else {
+            for (int i = 0; i < index; i++) {
                 temp = temp.next;
             }
             return temp;
 
         }
-   }
     }
 
+    public boolean setValue(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+}
 
--- src/testLinkedList.java
+++ src/testLinkedList.java
@@ -2,9 +2,12 @@ public class testLinkedList {
     public static void main(String[] args) {
         myLinkedList LL1 = new myLinkedList(5);
       LL1.prepend(6);
-      LL1.printLinkedList();
-        System.out.println(LL1.removeFirst().value);
-        System.out.println();
-      LL1.printLinkedList();
+      LL1.append(8);
+        System.out.print("Original Linked List :");
+        LL1.printLinkedList();
+        LL1.setValue(1,45);
+        LL1.printLinkedList();
+
+
     }
 }
