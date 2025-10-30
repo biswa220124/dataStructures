30aeaf67102ee60507a49b91c6ec5d4f85148ac8
biswa <biswabhusan2190@gmail.com>
2025-10-30 23:57:57 +0530

    Added new method for reversing an linked list

--- src/myLinkedList.java
+++ src/myLinkedList.java
@@ -145,5 +145,24 @@ public class myLinkedList {
 
 
     }
+    public void reverse(){
+        if(length==0) System.out.println("Empty");
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for(int i = 0; i<length; i++){
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+           temp  =after;
+
+
+
+
+
+        }
+    }
 }
 
