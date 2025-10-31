db58c5b5b3d44857b2dc4f63d222b7a21616623c
biswa <biswabhusan2190@gmail.com>
2025-10-31 19:30:05 +0530

    two sum problem from LEETCODE

---
+++ src/sum_2.java
@@ -0,0 +1,12 @@
+public class sum_2 {
+    public static int [] twoSum(int[] arr, int target){
+        for(int i = 0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
