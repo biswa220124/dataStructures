332045a207a05f635d4dcdd9e500d310b84da74a
biswa <biswabhusan2190@gmail.com>
2025-11-06 21:30:39 +0530

    Doubly Linked List Creation

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,19 @@
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+    class Node{
+        int value;
+        Node next;
+        Node prev;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    DoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length=1;
+    }
+}
