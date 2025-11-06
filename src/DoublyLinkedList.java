d3b59fe6a8fd352e09bda1a300862d15ee523532
biswa <biswabhusan2190@gmail.com>
2025-11-06 21:40:44 +0530

    Tester File For DoublyLinkedList

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -16,4 +16,21 @@ public class DoublyLinkedList {
         tail = newNode;
         length=1;
     }
+    public void printDoublyLinkedList(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    public void getHead(){
+        System.out.println("Head Value is: "+head.value);
+    }
+    public void getTail(){
+        System.out.println("Tail value is: "+tail.value);
+    }
+    public void getLength(){
+        System.out.println("Length is: "+length);
+    }
+
 }
---
+++ src/doublyLinkedListTester.java
@@ -0,0 +1,9 @@
+public class doublyLinkedListTester {
+    public static void main(String[] args) {
+        DoublyLinkedList DLL = new DoublyLinkedList(8);
+        DLL.printDoublyLinkedList();
+        DLL.getLength();
+        DLL.getHead();
+        DLL.getTail();
+    }
+}
